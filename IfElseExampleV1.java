5e1633657721a82c69b7afea4c9b0fa72556947f
gorrelacharishma <143474344+gorrelacharishma@users.noreply.github.com>
2023-09-18 21:06:42 -0700

    Add files via upload

---
+++ IfElseExampleV1.java
@@ -0,0 +1,13 @@
+public class IfElseExampleV1{
+    public static void main(String args[]){
+        int x=5;
+        int y=8;
+        if(x+y>20){
+            System.out.println("x+y is greater than 20");
+        }
+        else
+        {
+            System.out.println("x+y is less than 20");
+        }
+    }
+}
