4569bdd5ba3ec53a2de65c198a35db2c528733ae
gorrelacharishma <143474344+gorrelacharishma@users.noreply.github.com>
2023-09-25 12:01:58 +0530

    Add files via upload

---
+++ ThisConstructorDemo.java
@@ -0,0 +1,30 @@
+class A{
+   
+ A(){
+    
+      
+  System.out.println("Inside default constructor");
+   
+ }
+   
+ A(int x){
+     
+   this();
+     
+   System.out.println("Parameterized constructor");
+     
+   System.out.println("x value is " + x);
+  
+  }
+
+}
+ 
+public class ThisConstructorDemo{
+  
+  public static void main(String args[]){
+ 
+   A a=new A(4226);
+
+}
+
+}
