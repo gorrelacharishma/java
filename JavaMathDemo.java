627d6409bc0117248b1d81d3bd6d92b522b6edef
gorrelacharishma <143474344+gorrelacharishma@users.noreply.github.com>
2023-10-19 15:04:10 +0530

    Add files via upload

---
+++ JavaMathDemo.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+public class JavaMathDemo{
+    public static void main(String args[]){
+        double x=20;
+        double y=4;
+        System.out.println("Maximum of x and y is" + Math.max(x,y));
+        System.out.println("Minimum of x and y is"+ Math.min(x,y));
+        System.out.println("Sqrt of y is "+ Math.sqrt(y));
+        System.out.println("power of x and y is"+ Math.pow(x,y));
+    }
+}
